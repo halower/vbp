8ed99f3ffd389239d5c8ed13e3c2f2c84f607f78
halower <rohelm@163.com>
2017-11-01 14:29:45 +0800

    完善脚手架

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -11,9 +11,9 @@ const path = require('path')
 const express = require('express')
 const webpack = require('webpack')
 const proxyMiddleware = require('http-proxy-middleware')
-const webpackConfig = (process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'production')
+const webpackConfig = {{#if_or unit e2e}}(process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'production')
   ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
+  : {{/if_or}}require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
 const port = process.env.PORT || config.dev.port
