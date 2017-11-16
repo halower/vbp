d3692ea282182293ef0f5ff3756eebeb3e30b8a1
谢海龙 <xiehailong@163.com>
2017-11-16 15:53:48 +0800

    fixed demo page

--- template/src/pages/demo/HelloWorld.vue
+++ template/src/pages/demo/HelloWorld.vue
@@ -1,10 +1,10 @@
 <template>
  <div>
    <h3>\{{$t('test.title')}}</h3>
-    <h2>\{{$t('test.currentcode')}}：{{code}}</h2>
-    <h2>\{{$t('test.precode')}}: {{precode}}</h2>
-    <button @click="changeCode">{{$t('test.changecode')}}</button>
-    <button @click="alert">{{$t('test.simplealert')}}</button>
+    <h2>\{{$t('test.currentcode')}}：\{{code}}</h2>
+    <h2>\{{$t('test.precode')}}: \{{precode}}</h2>
+    <button @click="changeCode">\{{$t('test.changecode')}}</button>
+    <button @click="alert">\{{$t('test.simplealert')}}</button>
     <select v-model="lang" @change="$i18n.locale = lang">
        <option value='zh'>中文</option>
        <option value='en'>Engilsh</option>
