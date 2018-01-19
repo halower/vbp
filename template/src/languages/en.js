1f90cb05493da314cc48eaf3df589b44c5c4f1db
halower <121625933@qq.com>
2018-01-19 17:39:31 +0800

    Update en.js

--- template/src/languages/en.js
+++ template/src/languages/en.js
@@ -1,5 +1,5 @@
 module.exports = {
-  test: {
+  demo: {
     title: 'This page can be deleted in the actual development, this example is used to manage page status using vuex demo',
     currentcode: 'Current Code',
     precode: 'Previous Code',
